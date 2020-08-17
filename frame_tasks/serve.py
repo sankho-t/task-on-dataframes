e5b5d17fdddf1ac64d863e12976940e408c28fad
bob <bobatnet@gmail.com>
2020-08-17 21:37:31 +0530

    Add pagination to viewdata

--- frame_tasks/serve.py
+++ frame_tasks/serve.py
@@ -1,4 +1,5 @@
 import io
+from typing import List, Optional
 import pandas as pd
 import pkg_resources
 from flask import Flask, render_template, url_for, send_file
@@ -34,7 +35,7 @@ def explore(q=""):
 
     dataview_urls = []
     for ind, _ in enumerate(bs.vars):
-        u = url_for("view", q=q, index=ind)
+        u = url_for("view", q=q, index=ind, page=0)
         dataview_urls.append(u)
 
     act_links_q = []
@@ -80,19 +81,71 @@ class MyStyler(Styler):
     template = env.get_template("myhtml.tpl")
 
 
-@app.route("/view/<index>/<q>")
-def view(index: int, q: str):
+VIEW_MAX_COLWIDTH = 30
+PAGE_SIZE = 30
+NAV_PAGE_COUNT = 5
+
+
+def page_nav(cur: int, maxpage: int) -> List[int]:
+    start = max(cur - NAV_PAGE_COUNT // 2, 1)
+    stop = min(cur + NAV_PAGE_COUNT // 2, maxpage)
+    out = list(range(start, stop + 1))
+    if 0 not in out:
+        out.insert(0, 0)
+    if maxpage not in out:
+        out.append(maxpage)
+    return out
+
+
+from math import ceil
+
+
+@app.route("/view/<page>/<index>/<q>")
+def view(index: int, page: int, q: str):
     bs = BrowseState.from_url_q(q)
 
     if isinstance(index, str):
         index = int(index)
-    out = list(bs.real_outputs())[::-1]
-    index = min(len(out), index)
+    if isinstance(page, str):
+        if page.lower() == "first":
+            page = 0
+        elif page.lower() == "last":
+            page = -1
+        page = int(page)
+    out_ = list(bs.real_outputs())[::-1]
+    index = min(len(out_), index)
+    out = out_[index]
+
+    npages = ceil(out.shape[0] / PAGE_SIZE)
+
+    if page < 0:
+        page = npages + page
+    out = out.head(PAGE_SIZE * (page + 1)).tail(PAGE_SIZE)
+    nav_pages_name = [
+        {0: "First", npages - 1: "Last"}.get(x, str(x))
+        for x in page_nav(page, maxpage=npages - 1)
+    ]
+
+    nav_pages = dict(
+        zip(
+            nav_pages_name,
+            [
+                url_for("view", index=index, page=x, q=q)
+                for x in page_nav(page, maxpage=npages - 1)
+            ],
+        )
+    )
+    try:
+        current_page: Optional[int] = page_nav(page, maxpage=npages - 1).index(page)
+    except IndexError:
+        current_page = None
 
     escape_html_tags = (
         lambda x: x.replace("<", r"\<").replace(">", r"\>") if isinstance(x, str) else x
     )
-    data = pd.DataFrame(out[index]).applymap(escape_html_tags)
+    trim_col = lambda x: x[:30] + "..." if len(x) > 30 else x
+
+    data = pd.DataFrame(out).applymap(escape_html_tags).astype(str).applymap(trim_col)
 
     table = MyStyler(data).render()
 
@@ -101,7 +154,13 @@ def view(index: int, q: str):
         "Back": back_url,
         "Download as csv": url_for("download_csv", index=index, q=q),
     }
-    return render_template("viewdata.html", table=table, navs=navs)
+    return render_template(
+        "viewdata.html",
+        table=table,
+        navs=navs,
+        nav_pages=nav_pages,
+        current_page=current_page,
+    )
 
 
 @app.route("/download/csv/<index>/<q>")
